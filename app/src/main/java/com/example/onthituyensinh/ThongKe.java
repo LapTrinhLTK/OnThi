73c1d14e68d8f9dd435e2013ab24a44baeb2256f
Admin <laptrinhltk@gmail.com>
2020-11-05 08:08:53 +0700

    HongHong

--- app/src/main/java/com/example/onthituyensinh/ThongKe.java
+++ app/src/main/java/com/example/onthituyensinh/ThongKe.java
@@ -2,13 +2,25 @@ package com.example.onthituyensinh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThongKe extends AppCompatActivity {
-
+    Button btnvtt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thong_ke);
+
+        btnvtt = (Button) findViewById(R.id.btnbackmain);
+        btnvtt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chuyenmain4 = new Intent(ThongKe.this, MainActivity.class);
+                startActivity(chuyenmain4);
+            }
+        });
     }
 }
